e2a7edf1b2650cfb8ab4eef888c56c8a321274cd
Maureen <mautich06gmail.com>
2024-05-24 07:10:09 -0700

    divide a list

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -13,7 +13,7 @@ def list_division(my_list_1, my_list_2, list_length):
             print("division by 0")
         except IndexError:
             quotient = 0
-            print ("out of range")
+            print("out of range")
         finally:
             new_list.append(quotient)
     return new_list
