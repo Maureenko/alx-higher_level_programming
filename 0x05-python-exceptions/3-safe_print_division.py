22fb8c867b7913c46d639366449b0b842c40997f
Maureen <mautich06gmail.com>
2024-05-24 06:03:12 -0700

    integer division with debug

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+def safe_print_division(a, b):
+    try:
+        quotient = a / b
+    except (ZeroDivisionError):
+        quotient = None
+    finally:
+        print("Inside result: {}".format(quotient))
+        return quotient
