e83b532a6be200d695b2a28fe1bffd6ba47ad304
Maureen Rotich <mautich06@gmail.com>
2024-08-03 14:53:32 +0300

    is kind of in the same class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!usr/bin/python3
+"""mod documentation"""
+
+
+def is_kind_of_class(obj, a_class):
+    """checks if object is instance of class or its base class"""
+
+    return isinstance(obj, a_class)
