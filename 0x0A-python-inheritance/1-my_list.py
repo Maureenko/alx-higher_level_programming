f669de2f6f65bf214fdb4c952f417afd3f3deae5
Maureen Rotich <mautich06@gmail.com>
2024-08-03 12:29:57 +0300

    ist of available attributes and methods of an object.

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""" Defines a class MyList that inherits from list."""
+
+class MyList(list):
+    def print_sorted(self):
+        listOb = sorted(self.list)
+        print (listOb)
