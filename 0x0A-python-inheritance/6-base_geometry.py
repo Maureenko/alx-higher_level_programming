cc73ed4b43c4b4dc89de24f7a91da3701f746aef
Maureen Rotich <mautich06@gmail.com>
2024-08-05 23:03:38 +0300

    improved geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""mod documentation"""
+
+
+class BaseGeometry():
+    """improved BaseGeometry class"""
+
+    def area(self):
+        """method that raises error"""
+
+        raise NotImplementedError("area() is not implemented")
+    
