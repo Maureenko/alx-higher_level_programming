d4c438af91cac61e00f59c5241bb8695b6851f0f
Maureen Rotich <mautich06@gmail.com>
2024-08-27 00:42:44 +0300

    rectangle.py

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""defines a class rectangle"""
+
+
+class Rectangle:
+    """Class rectangle with attributes."""
+
+    def __init__(self, width=0, height=0):
+        """initialization of attributes width and height"""
+
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """getter for the width"""
+
+        return self.__width
+
+
+    @width.setter
+    def width(self, value):
+        """setter for the width"""
+
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+
+    @property
+    def height(self):
+        """getter for the height"""
+
+        return self.__width
+
+
+    @height.setter
+    def height(self, value):
+        """setter for the height"""
+
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+more problems on classes in python
