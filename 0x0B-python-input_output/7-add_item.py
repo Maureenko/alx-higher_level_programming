f718bfd0b1e755b4783e8e9463f439c0b701e847
Maureen Rotich <mautich06@gmail.com>
2024-09-26 11:59:08 +0300

    input output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-""" adds all arguments to a python list then
-    saves to a file"""
+"""Script that adds all arguments to a Python list and saves them to a file."""
 import sys
 
-
+# Importing the functions from the previous tasks
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
+
+# Try to load the list from the JSON file, or create an empty list if it doesn't exist
 try:
     my_list = load_from_json_file(filename)
 except FileNotFoundError:
     my_list = []
 
-for i in range(1, len(sys.argv)):
-    my_list.append(sys.argv[i])
+# Append command-line arguments to the list
+my_list.extend(sys.argv[1:])
 
+# Save the updated list back to the file
 save_to_json_file(my_list, filename)
